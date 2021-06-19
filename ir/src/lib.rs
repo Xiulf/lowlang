9800f119590e3249998c737c1aecf7ce56ff1138
Cyberduc-k <tttymo@gmail.com>
2021-06-19 12:27:04 +0200

    basic ir

--- ir/src/lib.rs
+++ ir/src/lib.rs
@@ -1,7 +1,24 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use arena::{Arena, Idx};
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Body {
+    pub vars: Arena<VarInfo>,
+    pub blocks: Arena<Block>,
+}
+
+pub type Var = Idx<VarInfo>;
+pub type BlockId = Idx<Block>;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct VarInfo {}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Block {
+    pub instrs: Vec<Instr>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Instr {
+    BoxAlloc {},
+    BoxFree {},
 }
