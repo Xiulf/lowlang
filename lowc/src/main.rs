bdca8eea62b366237c8691203dafd25e7bb25c81
Cyberduc-k <tttymo@gmail.com>
2021-06-24 00:27:24 +0200

    use lld linker

--- lowc/src/main.rs
+++ lowc/src/main.rs
@@ -9,16 +9,19 @@ fn main() {
     println!("{}", module);
 
     let obj_file = codegen::compile_module(&module);
+    let _ = std::fs::copy(obj_file.path(), "test.o").unwrap();
+    let link_args = ["test.o", "runtime/target/runtime.so", "-l", "c", "-o", "test"];
+    let link_args = link_args.iter().map(<&str>::to_string).collect::<Vec<_>>();
 
-    std::fs::copy(obj_file.path(), "test.o").unwrap();
+    mun_lld::link(mun_lld::LldFlavor::Elf, &link_args).ok().unwrap();
 
-    std::process::Command::new("cc")
-        .arg("test.o")
-        .arg("runtime/target/runtime.so")
-        .arg("-o")
-        .arg("test")
-        .status()
-        .unwrap();
+    // std::process::Command::new("cc")
+    //     .arg("test.o")
+    //     .arg("runtime/target/runtime.so")
+    //     .arg("-o")
+    //     .arg("test")
+    //     .status()
+    //     .unwrap();
 
     std::fs::remove_file("test.o").unwrap();
 }
