e3a710ad6527b8d51fd7a73f73f9c29da576cc59
Cyberduc-k <tttymo@gmail.com>
2022-01-11 10:06:30 +0100

    replace tabs with spaces

--- src/lib.rs
+++ src/lib.rs
@@ -5,79 +5,79 @@ pub mod type_info;
 use type_info::*;
 
 struct Pair<T> {
-	x: T,
-	y: T,
+    x: T,
+    y: T,
 }
 
 struct TypeInfoPair {
-	vwt: *const ValueWitnessTable,
-	flags: usize,
-	T: *const TypeInfo,
-	x: usize,
-	y: usize,
+    vwt: *const ValueWitnessTable,
+    flags: usize,
+    T: *const TypeInfo,
+    x: usize,
+    y: usize,
 }
 
 unsafe extern "C" fn copy_pair(dst: *mut u8, src: *const u8, TP: *const TypeInfoPair) {
-	let T = (*TP).T;
-	let x = (*TP).x;
-	let y = (*TP).y;
+    let T = (*TP).T;
+    let x = (*TP).x;
+    let y = (*TP).y;
 
-	((*(*T).vwt).copy_fn)(dst.add(x), src.add(x), T);
-	((*(*T).vwt).copy_fn)(dst.add(y), src.add(y), T);
+    ((*(*T).vwt).copy_fn)(dst.add(x), src.add(x), T);
+    ((*(*T).vwt).copy_fn)(dst.add(y), src.add(y), T);
 }
 
 unsafe extern "C" fn move_pair(dst: *mut u8, src: *const u8, TP: *const TypeInfoPair) {
-	let T = (*TP).T;
-	let x = (*TP).x;
-	let y = (*TP).y;
+    let T = (*TP).T;
+    let x = (*TP).x;
+    let y = (*TP).y;
 
-	((*(*T).vwt).move_fn)(dst.add(x), src.add(x), T);
-	((*(*T).vwt).move_fn)(dst.add(y), src.add(y), T);
+    ((*(*T).vwt).move_fn)(dst.add(x), src.add(x), T);
+    ((*(*T).vwt).move_fn)(dst.add(y), src.add(y), T);
 }
 
 unsafe extern "C" fn drop_pair(val: *mut u8, TP: *const TypeInfoPair) {
-	let T = (*TP).T;
-	let x = (*TP).x;
-	let y = (*TP).y;
+    let T = (*TP).T;
+    let x = (*TP).x;
+    let y = (*TP).y;
 
-	((*(*T).vwt).drop_fn)(val.add(x), T);
-	((*(*T).vwt).drop_fn)(val.add(y), T);
+    ((*(*T).vwt).drop_fn)(val.add(x), T);
+    ((*(*T).vwt).drop_fn)(val.add(y), T);
 }
 
 unsafe fn mk_pair_vwt(T: *const TypeInfo) -> ValueWitnessTable {
-	if (*T).flags & FLAG_TRIVIAL == 1 {
-		ValueWitnessTable {
-			size: (*(*T).vwt).size * 2,
-			align: (*(*T).vwt).align,
-			stride: (*(*T).vwt).stride * 2,
-			copy_fn: copy_trivial,
-			move_fn: move_trivial,
-			drop_fn: drop_trivial,
-		}
-	} else {
-		let copy_pair = copy_pair as unsafe extern "C" fn(*mut u8, *const u8, *const TypeInfoPair);
-		let move_pair = move_pair as unsafe extern "C" fn(*mut u8, *const u8, *const TypeInfoPair);
-		let drop_pair = drop_pair as unsafe extern "C" fn(*mut u8, *const TypeInfoPair);
+    if (*T).flags & FLAG_TRIVIAL == 1 {
+        ValueWitnessTable {
+            size: (*(*T).vwt).size * 2,
+            align: (*(*T).vwt).align,
+            stride: (*(*T).vwt).stride * 2,
+            copy_fn: copy_trivial,
+            move_fn: move_trivial,
+            drop_fn: drop_trivial,
+        }
+    } else {
+        let copy_pair = copy_pair as unsafe extern "C" fn(*mut u8, *const u8, *const TypeInfoPair);
+        let move_pair = move_pair as unsafe extern "C" fn(*mut u8, *const u8, *const TypeInfoPair);
+        let drop_pair = drop_pair as unsafe extern "C" fn(*mut u8, *const TypeInfoPair);
 
-		ValueWitnessTable {
-			size: (*(*T).vwt).size * 2,
-			align: (*(*T).vwt).align,
-			stride: (*(*T).vwt).stride * 2,
-			copy_fn: ::std::mem::transmute(copy_pair),
-			move_fn: ::std::mem::transmute(move_pair),
-			drop_fn: ::std::mem::transmute(drop_pair),
-		}
-	}
+        ValueWitnessTable {
+            size: (*(*T).vwt).size * 2,
+            align: (*(*T).vwt).align,
+            stride: (*(*T).vwt).stride * 2,
+            copy_fn: ::std::mem::transmute(copy_pair),
+            move_fn: ::std::mem::transmute(move_pair),
+            drop_fn: ::std::mem::transmute(drop_pair),
+        }
+    }
 }
 
 unsafe fn mk_pair_ti(vwt: *const ValueWitnessTable, T: *const TypeInfo) -> TypeInfoPair {
-	TypeInfoPair {
-		vwt,
-		T,
-		flags: (*T).flags,
-		x: 0,
-		y: (*(*T).vwt).stride,
-	}
+    TypeInfoPair {
+        vwt,
+        T,
+        flags: (*T).flags,
+        x: 0,
+        y: (*(*T).vwt).stride,
+    }
 }
 
 ///	export identity : $<type T>([in] T) -> [out] T
@@ -89,10 +89,10 @@ unsafe fn mk_pair_ti(vwt: *const ValueWitnessTable, T: *const TypeInfo) -> TypeI
 ///		return
 ///	}
 unsafe fn identity(ret: *mut u8, x: *mut u8, T: *const TypeInfo) {
-	((*(*T).vwt).move_fn)(ret, x, T);
+    ((*(*T).vwt).move_fn)(ret, x, T);
 }
 
-/// export second : $<type T>([in] Pair<$T>) -> [out] T
+///	export second : $<type T>([in] Pair<$T>) -> [out] T
 ///	
 ///	body second {
 ///	
@@ -103,12 +103,12 @@ unsafe fn identity(ret: *mut u8, x: *mut u8, T: *const TypeInfo) {
 ///		return
 ///	}
 unsafe fn second(ret: *mut u8, pair: *mut u8, T: *const TypeInfo) {
-	let TP_VWT = mk_pair_vwt(T);
-	let TP = mk_pair_ti(&TP_VWT, T);
-	let field = pair.add((*(*T).vwt).stride);
+    let TP_VWT = mk_pair_vwt(T);
+    let TP = mk_pair_ti(&TP_VWT, T);
+    let field = pair.add((*(*T).vwt).stride);
 
-	((*(*T).vwt).move_fn)(ret, field, T);
-	((*TP.vwt).drop_fn)(pair, &TP as *const _ as *const TypeInfo);
+    ((*(*T).vwt).move_fn)(ret, field, T);
+    ((*TP.vwt).drop_fn)(pair, &TP as *const _ as *const TypeInfo);
 }
 
 ///	export pair : $<type T>([in] T) -> [out] Pair<$T>
@@ -123,8 +123,8 @@ unsafe fn second(ret: *mut u8, pair: *mut u8, T: *const TypeInfo) {
 ///		return
 ///	}
 unsafe fn pair(ret: *mut u8, x: *mut u8, T: *const TypeInfo) {
-	((*(*T).vwt).copy_fn)(ret, x, T);
-	((*(*T).vwt).move_fn)(ret.add((*(*T).vwt).stride), x, T);
+    ((*(*T).vwt).copy_fn)(ret, x, T);
+    ((*(*T).vwt).move_fn)(ret.add((*(*T).vwt).stride), x, T);
 }
 
 ///	export combo : $<type T>([in] T) -> [out] Pair<$T>
@@ -141,73 +141,73 @@ unsafe fn pair(ret: *mut u8, x: *mut u8, T: *const TypeInfo) {
 ///		return
 ///	}
 unsafe fn combo(ret: *mut u8, x: *mut u8, T: *const TypeInfo) {
-	let TP_VWT = mk_pair_vwt(T);
-	let TP = mk_pair_ti(&TP_VWT, T);
+    let TP_VWT = mk_pair_vwt(T);
+    let TP = mk_pair_ti(&TP_VWT, T);
 
-	let tmp = ::std::alloc::alloc(::std::alloc::Layout::from_size_align(
-		(*TP.vwt).size,
-		(*TP.vwt).align,
-	).unwrap());
+    let tmp = ::std::alloc::alloc(::std::alloc::Layout::from_size_align(
+        (*TP.vwt).size,
+        (*TP.vwt).align,
+    ).unwrap());
 
-	pair(tmp, x, T);
-	identity(ret, tmp, &TP as *const _ as *const TypeInfo);
+    pair(tmp, x, T);
+    identity(ret, tmp, &TP as *const _ as *const TypeInfo);
 
-	::std::alloc::dealloc(tmp, ::std::alloc::Layout::from_size_align(
-		(*TP.vwt).size,
-		(*TP.vwt).align,
-	).unwrap());
+    ::std::alloc::dealloc(tmp, ::std::alloc::Layout::from_size_align(
+        (*TP.vwt).size,
+        (*TP.vwt).align,
+    ).unwrap());
 }
 
 #[test]
 fn test_identity() {
-	let i32_info = &TRIVIAL_TIS[3];
-	let mut ret: i32 = 0;
-	let arg: i32 = 124;
+    let i32_info = &TRIVIAL_TIS[3];
+    let mut ret: i32 = 0;
+    let arg: i32 = 124;
 
-	unsafe {
-		identity(&mut ret as *mut _ as *mut u8, &arg as *const _ as *mut u8, i32_info);
-	}
+    unsafe {
+        identity(&mut ret as *mut _ as *mut u8, &arg as *const _ as *mut u8, i32_info);
+    }
 
-	assert_eq!(ret, 124);
+    assert_eq!(ret, 124);
 }
 
 #[test]
 fn test_second() {
-	let i32_info = &TRIVIAL_TIS[3];
-	let mut ret: i32 = 0;
-	let arg: Pair<i32> = Pair { x: 24, y: 12 };
+    let i32_info = &TRIVIAL_TIS[3];
+    let mut ret: i32 = 0;
+    let arg: Pair<i32> = Pair { x: 24, y: 12 };
 
-	unsafe {
-		second(&mut ret as *mut _ as *mut u8, &arg as *const _ as *mut u8, i32_info);
-	}
+    unsafe {
+        second(&mut ret as *mut _ as *mut u8, &arg as *const _ as *mut u8, i32_info);
+    }
 
-	assert_eq!(ret, 12);
+    assert_eq!(ret, 12);
 }
 
 #[test]
 fn test_pair() {
-	let i32_info = &TRIVIAL_TIS[3];
-	let mut ret: Pair<i32> = Pair { x: 0, y: 0 };
-	let arg: i32 = 17;
+    let i32_info = &TRIVIAL_TIS[3];
+    let mut ret: Pair<i32> = Pair { x: 0, y: 0 };
+    let arg: i32 = 17;
 
-	unsafe {
-		pair(&mut ret as *mut _ as *mut u8, &arg as *const _ as *mut u8, i32_info);
-	}
+    unsafe {
+        pair(&mut ret as *mut _ as *mut u8, &arg as *const _ as *mut u8, i32_info);
+    }
 
-	assert_eq!(ret.x, 17);
-	assert_eq!(ret.y, 17);
+    assert_eq!(ret.x, 17);
+    assert_eq!(ret.y, 17);
 }
 
 #[test]
 fn test_combo() {
-	let i32_info = &TRIVIAL_TIS[3];
-	let mut ret: Pair<i32> = Pair { x: 0, y: 0 };
-	let arg: i32 = 17;
+    let i32_info = &TRIVIAL_TIS[3];
+    let mut ret: Pair<i32> = Pair { x: 0, y: 0 };
+    let arg: i32 = 17;
 
-	unsafe {
-		combo(&mut ret as *mut _ as *mut u8, &arg as *const _ as *mut u8, i32_info);
-	}
+    unsafe {
+        combo(&mut ret as *mut _ as *mut u8, &arg as *const _ as *mut u8, i32_info);
+    }
 
-	assert_eq!(ret.x, 17);
-	assert_eq!(ret.y, 17);
+    assert_eq!(ret.x, 17);
+    assert_eq!(ret.y, 17);
 }
--- src/type_info.rs
+++ src/type_info.rs
@@ -1,28 +1,28 @@
 #[repr(C)]
 pub struct TypeInfo {
-	pub vwt: *const ValueWitnessTable,
-	pub flags: usize,
+    pub vwt: *const ValueWitnessTable,
+    pub flags: usize,
 }
 
 #[repr(C)]
 pub struct ValueWitnessTable {
-	pub size: usize,
-	pub align: usize,
-	pub stride: usize,
-	pub copy_fn: unsafe extern "C" fn(*mut u8, *const u8, *const TypeInfo),
-	pub move_fn: unsafe extern "C" fn(*mut u8, *const u8, *const TypeInfo),
-	pub drop_fn: unsafe extern "C" fn(*mut u8, *const TypeInfo),
+    pub size: usize,
+    pub align: usize,
+    pub stride: usize,
+    pub copy_fn: unsafe extern "C" fn(*mut u8, *const u8, *const TypeInfo),
+    pub move_fn: unsafe extern "C" fn(*mut u8, *const u8, *const TypeInfo),
+    pub drop_fn: unsafe extern "C" fn(*mut u8, *const TypeInfo),
 }
 
 unsafe impl Sync for TypeInfo {}
 unsafe impl Sync for ValueWitnessTable {}
 
 pub unsafe extern "C" fn copy_trivial(dst: *mut u8, src: *const u8, ti: *const TypeInfo) {
-	::std::ptr::copy_nonoverlapping(src, dst, (*(*ti).vwt).size);
+    ::std::ptr::copy_nonoverlapping(src, dst, (*(*ti).vwt).size);
 }
 
 pub unsafe extern "C" fn move_trivial(dst: *mut u8, src: *const u8, ti: *const TypeInfo) {
-	::std::ptr::copy(src, dst, (*(*ti).vwt).size);
+    ::std::ptr::copy(src, dst, (*(*ti).vwt).size);
 }
 
 pub unsafe extern "C" fn copy_move_nop(_dst: *mut u8, _src: *const u8, _ti: *const TypeInfo) {}
@@ -32,79 +32,79 @@ pub unsafe extern "C" fn drop_trivial(_val: *mut u8, _ti: *const TypeInfo) {}
 pub const FLAG_TRIVIAL: usize = 1 << 0;
 
 pub static TRIVIAL_TIS: [TypeInfo; 6] = [
-	TypeInfo {
-		vwt: &TRIVIAL_VWTS[0],
-		flags: FLAG_TRIVIAL,
-	},
-	TypeInfo {
-		vwt: &TRIVIAL_VWTS[1],
-		flags: FLAG_TRIVIAL,
-	},
-	TypeInfo {
-		vwt: &TRIVIAL_VWTS[2],
-		flags: FLAG_TRIVIAL,
-	},
-	TypeInfo {
-		vwt: &TRIVIAL_VWTS[3],
-		flags: FLAG_TRIVIAL,
-	},
-	TypeInfo {
-		vwt: &TRIVIAL_VWTS[4],
-		flags: FLAG_TRIVIAL,
-	},
-	TypeInfo {
-		vwt: &TRIVIAL_VWTS[5],
-		flags: FLAG_TRIVIAL,
-	},
+    TypeInfo {
+        vwt: &TRIVIAL_VWTS[0],
+        flags: FLAG_TRIVIAL,
+    },
+    TypeInfo {
+        vwt: &TRIVIAL_VWTS[1],
+        flags: FLAG_TRIVIAL,
+    },
+    TypeInfo {
+        vwt: &TRIVIAL_VWTS[2],
+        flags: FLAG_TRIVIAL,
+    },
+    TypeInfo {
+        vwt: &TRIVIAL_VWTS[3],
+        flags: FLAG_TRIVIAL,
+    },
+    TypeInfo {
+        vwt: &TRIVIAL_VWTS[4],
+        flags: FLAG_TRIVIAL,
+    },
+    TypeInfo {
+        vwt: &TRIVIAL_VWTS[5],
+        flags: FLAG_TRIVIAL,
+    },
 ];
 
 pub static TRIVIAL_VWTS: [ValueWitnessTable; 6] = [
-	ValueWitnessTable {
-		size: 0,
-		align: 1,
-		stride: 0,
-		copy_fn: copy_move_nop,
-		move_fn: copy_move_nop,
-		drop_fn: drop_trivial,
-	},
-	ValueWitnessTable {
-		size: 1,
-		align: 1,
-		stride: 1,
-		copy_fn: copy_trivial,
-		move_fn: move_trivial,
-		drop_fn: drop_trivial,
-	},
-	ValueWitnessTable {
-		size: 2,
-		align: 2,
-		stride: 2,
-		copy_fn: copy_trivial,
-		move_fn: move_trivial,
-		drop_fn: drop_trivial,
-	},
-	ValueWitnessTable {
-		size: 4,
-		align: 4,
-		stride: 4,
-		copy_fn: copy_trivial,
-		move_fn: move_trivial,
-		drop_fn: drop_trivial,
-	},
-	ValueWitnessTable {
-		size: 8,
-		align: 8,
-		stride: 8,
-		copy_fn: copy_trivial,
-		move_fn: move_trivial,
-		drop_fn: drop_trivial,
-	},
-	ValueWitnessTable {
-		size: 16,
-		align: 8,
-		stride: 16,
-		copy_fn: copy_trivial,
-		move_fn: move_trivial,
-		drop_fn: drop_trivial,
-	},
+    ValueWitnessTable {
+        size: 0,
+        align: 1,
+        stride: 0,
+        copy_fn: copy_move_nop,
+        move_fn: copy_move_nop,
+        drop_fn: drop_trivial,
+    },
+    ValueWitnessTable {
+        size: 1,
+        align: 1,
+        stride: 1,
+        copy_fn: copy_trivial,
+        move_fn: move_trivial,
+        drop_fn: drop_trivial,
+    },
+    ValueWitnessTable {
+        size: 2,
+        align: 2,
+        stride: 2,
+        copy_fn: copy_trivial,
+        move_fn: move_trivial,
+        drop_fn: drop_trivial,
+    },
+    ValueWitnessTable {
+        size: 4,
+        align: 4,
+        stride: 4,
+        copy_fn: copy_trivial,
+        move_fn: move_trivial,
+        drop_fn: drop_trivial,
+    },
+    ValueWitnessTable {
+        size: 8,
+        align: 8,
+        stride: 8,
+        copy_fn: copy_trivial,
+        move_fn: move_trivial,
+        drop_fn: drop_trivial,
+    },
+    ValueWitnessTable {
+        size: 16,
+        align: 8,
+        stride: 16,
+        copy_fn: copy_trivial,
+        move_fn: move_trivial,
+        drop_fn: drop_trivial,
+    },
 ];
